2ebd171b2e934095673188a99caccd1058fe2780
sonam-niit <sonam.gravity@gmail.com>
2023-12-19 19:51:05 +0530

    Links updated

--- jwttokenfrontend/src/pages/Register.js
+++ jwttokenfrontend/src/pages/Register.js
@@ -10,7 +10,7 @@ function Register() {
         e.preventDefault();
         //console.log(user);
         try {
-            const resp = await axios.post('https://jwtdemo.onrender.com/auth/register', user);
+            const resp = await axios.post('https://jwtdemo.onrender.com/api/auth/register', user);
             //console.log(resp);
             if (resp.status == 201) {
                 // alert(resp.data.message);
