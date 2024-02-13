dbe97893b09979281348a1a19675040220760b11
sonam-niit <sonam.gravity@gmail.com>
2024-02-13 20:08:11 +0530

    cors configured

--- RestFullAPI-MongoDB/Backend/index.js
+++ RestFullAPI-MongoDB/Backend/index.js
@@ -5,6 +5,7 @@ require('./models/dbconnect');
 //configure Dot ENV
 dotenv.config();
 const app = express();
+app.use(require('cors')())
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); //JSON Parser
 //temp check
